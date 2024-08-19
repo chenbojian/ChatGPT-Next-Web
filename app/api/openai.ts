dd429bbd2c2b4dd5df58feee760d1246d79416ff
chenbojian <bjchen@thoughtworks.com>
2024-08-19 14:18:06 +0800

    hack to support databricks

--- app/api/openai.ts
+++ app/api/openai.ts
@@ -1,30 +1,15 @@
-import { type OpenAIListModelResponse } from "@/app/client/platforms/openai";
 import { getServerSideConfig } from "@/app/config/server";
-import { ModelProvider, OpenaiPath } from "@/app/constant";
+import { ModelProvider } from "@/app/constant";
 import { prettyObject } from "@/app/utils/format";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./auth";
 import { requestOpenai } from "./common";
 
-const ALLOWD_PATH = new Set(Object.values(OpenaiPath));
-
-function getModels(remoteModelRes: OpenAIListModelResponse) {
-  const config = getServerSideConfig();
-
-  if (config.disableGPT4) {
-    remoteModelRes.data = remoteModelRes.data.filter(
-      (m) => !m.id.startsWith("gpt-4") || m.id.startsWith("gpt-4o-mini"),
-    );
-  }
-
-  return remoteModelRes;
-}
-
 export async function handle(
   req: NextRequest,
   { params }: { params: { path: string[] } },
 ) {
-  console.log("[OpenAI Route] params ", params);
+  console.log("[Databricks Route] params ", params);
 
   if (req.method === "OPTIONS") {
     return NextResponse.json({ body: "OK" }, { status: 200 });
@@ -32,41 +17,27 @@ export async function handle(
 
   const subpath = params.path.join("/");
 
-  if (!ALLOWD_PATH.has(subpath)) {
-    console.log("[OpenAI Route] forbidden path ", subpath);
-    return NextResponse.json(
-      {
-        error: true,
-        msg: "you are not allowed to request " + subpath,
-      },
-      {
-        status: 403,
-      },
-    );
-  }
-
-  const authResult = auth(req, ModelProvider.GPT);
-  if (authResult.error) {
-    return NextResponse.json(authResult, {
-      status: 401,
-    });
-  }
+  const token = req.headers
+    .get("Authorization")
+    ?.trim()
+    .replaceAll("Bearer ", "")
+    .trim();
+  const req_json = await req.json();
 
   try {
-    const response = await requestOpenai(req);
-
-    // list models
-    if (subpath === OpenaiPath.ListModelPath && response.status === 200) {
-      const resJson = (await response.json()) as OpenAIListModelResponse;
-      const availableModels = getModels(resJson);
-      return NextResponse.json(availableModels, {
-        status: response.status,
-      });
-    }
-
-    return response;
+    return await fetch(
+      `https://adb-6192355565634015.15.azuredatabricks.net/serving-endpoints/${req_json["model"]}/invocations`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + btoa(`token:${token}`),
+        },
+        body: JSON.stringify(req_json),
+      },
+    );
   } catch (e) {
-    console.error("[OpenAI] ", e);
+    console.error("[Azure] ", e);
     return NextResponse.json(prettyObject(e));
   }
 }
